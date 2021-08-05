73c38396b6da8c5b2dbc0948d68206cfa3dcf250
jitesh kumar <kumar48jitesh@gmail.com>
2021-08-05 23:52:07 +0530

    amazone server, amazon shopping and payment service added

---
+++ amezon-eureka-server/src/test/java/com/javainfotech/eurekaserver/AmezonEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainfotech.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmezonEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/test/java/com/javainfotech/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainfotech.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
